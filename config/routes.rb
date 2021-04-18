5a068401cdc979f9f5223a0fb5dbf19d895e3c9a
spiderweb0802 <81157779+bkrcap@users.noreply.github.com>
2021-04-18 15:18:20 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  #devise_scope :user do
+    #get '/users/sign_out' => 'devise/sessions#destroy'
+  #end
+  devise_for :users
+  get 'prototypes/index'
+  root to: "prototypes#index"
+
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: :create
+  end
+
+  resources :users, only: :show
+  
+end
