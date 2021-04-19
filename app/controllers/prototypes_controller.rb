0efeaccf19299be8e47b93c98c804ebaaf0dd62a
spiderweb0802 <81157779+bkrcap@users.noreply.github.com>
2021-04-19 17:35:52 +0900

    編集ボタンで編集画面に遷移できないことを修正(before_actionの順番を変更)、及び非ログインユーザーでもプロトタイプ一覧が表示されるようにする修正の一部(expectをexceptと修正)

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,8 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, expect: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  
   
   def index
     @prototypes = Prototype.includes(:user)
@@ -22,17 +23,22 @@ class PrototypesController < ApplicationController
   end
 
    def show
-    @prototype = Prototype.find(params[:id])
+    # @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments
    end
 
   def edit
-    @prototype = Prototype.find(params[:id])
+    
+    # @prototype = Prototype.find(params[:id])
+    #投稿者以外のユーザーが、投稿者専用のページに遷移できないようにしたい時は下記
+    #unless @prototype.user_id == current_user.id
+      #editさせない処理
+    #end
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
+    # @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
@@ -41,7 +47,7 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
+    # @prototype = Prototype.find(params[:id])
     if @prototype.destroy
       redirect_to root_path
     else
@@ -59,6 +65,7 @@ class PrototypesController < ApplicationController
   end
 
   def contributor_confirmation
+    # binding.pry
     redirect_to root_path unless current_user == @prototype.user
   end
 end
